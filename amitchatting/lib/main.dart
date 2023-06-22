393b49c05782652b1b9e218c651391c6dcb5b26b
yasserelsawy2 <yasserelsawy4@gmail.com>
2023-06-22 10:47:45 +0200

    notification

--- amitchatting/lib/main.dart
+++ amitchatting/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:groupchatapp/myapp.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -9,5 +11,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onMessage.listen((event) {
+    showSimpleNotification(Text(event.notification!.title!),
+        subtitle: Text(event.notification!.body!), background: Colors.green);
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    showSimpleNotification(Text(event.notification!.title!),
+        subtitle: Text(event.notification!.body!), background: Colors.green);
+  });
   runApp(const MyApp());
 }
--- amitchatting/lib/myapp.dart
+++ amitchatting/lib/myapp.dart
@@ -3,20 +3,23 @@ import 'package:groupchatapp/Features/Register/presentaion/registerscreen.dart';
 import 'package:groupchatapp/Features/chatscreen/presentation/chatscreen.dart';
 import 'package:groupchatapp/Features/login/presentation/loginscreen.dart';
 import 'package:groupchatapp/core/Stringsfile.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        Strings.loginPageId: (context) => LoginPage(),
-        Strings.registerPageId: (context) => RegisterPage(),
-        Strings.chatPageId: (context) => ChatPage()
-      },
-      initialRoute: Strings.loginPageId,
+    return OverlaySupport(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          Strings.loginPageId: (context) => LoginPage(),
+          Strings.registerPageId: (context) => RegisterPage(),
+          Strings.chatPageId: (context) => ChatPage()
+        },
+        initialRoute: Strings.loginPageId,
+      ),
     );
   }
 }
